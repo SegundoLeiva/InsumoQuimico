e04c3499e67cf15bc257246be6cc4873edb16d09
7ea0 <7ea0@192.168.1.14>
2016-11-05 12:31:05 -0500

    Correcciones

--- InsumoQuimico/src/main/java/controller/MarcaController.java
+++ InsumoQuimico/src/main/java/controller/MarcaController.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = "/marca")
 public class MarcaController {
-	protected final Log logger = LogFactory.getLog(getClass());
-//	@Autowired
-//	private MarcaDao marcaDao;
-
 
 	@RequestMapping(value = "/verMarcas.htm")
 	public String verMarcas(HttpSession sesion, Model model)
 			throws ServletException, IOException {
-//		model.addAttribute("listaMarcas", this.marcaDao.getlistamaetab(2));
 		model.addAttribute("index", "2");
 		return "verMarcas";
 	}
--- InsumoQuimico/src/main/webapp/WEB-INF/templateSistema/system.jsp
+++ InsumoQuimico/src/main/webapp/WEB-INF/templateSistema/system.jsp
@@ -4,7 +4,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8" />
-		<title>Sistema Inventario</title>
+		<title>Sistema Insumo Químico</title>
 		<meta name="description" content="overview &amp; stats" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
