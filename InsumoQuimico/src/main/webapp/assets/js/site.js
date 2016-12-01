ef9623b75383b26b503494da3cbf25a616eb9e44
segundo.leiva <segundo.leiva@PE-L00425.hundred.com.pe>
2016-12-01 14:50:55 -0500

    mejora site

--- InsumoQuimico/src/main/webapp/assets/js/site.js
+++ InsumoQuimico/src/main/webapp/assets/js/site.js
@@ -14,38 +14,20 @@ $(document).on("ready", function() {
 });
 
 function validarCamposRequeridos(idFormulario){
-	if(!inputRequeridos(idFormulario)){
-		return false;
-	}
-	if(!selectRequeridos(idFormulario)){
+	if(!camposRequeridos(idFormulario)){
 		return false;
 	}
 	return true;
 }
 
-function inputRequeridos(idFormulario){
+function camposRequeridos(idFormulario){
 	var rpta = true;
-	var formInput = $("#"+idFormulario+" input");
+	var formInput = $("#"+idFormulario);
 	$.each( formInput, function(index, value ) {
 		  if($(formInput[index]).prop("required")){
 			  if($(this).val()==""){
 				 alertify.error($(this).data("msg-required"));
 				 rpta = false;
-				 return false;
-				  }
-			}
-	});
-	return rpta;
-}
-
-function selectRequeridos(idFormulario){
-	var rpta = true;
-	var formSelect = $("#"+idFormulario+" select");
-	$.each( formSelect, function(index, value ) {
-		  if($(formSelect[index]).prop("required")){
-			  if($(this).val()==""){
-				 alertify.error($(this).data("msg-required"));
-				 rpta = false;	
 				 return false;
 				  }
 			}
