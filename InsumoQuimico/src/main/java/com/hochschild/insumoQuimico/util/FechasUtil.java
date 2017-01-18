73de1266510585e854c03e0a248cc429fe53218d
segundo.leiva <segundo.leiva@PE-L00425.hundred.com.pe>
2017-01-18 11:05:05 -0500

    mejoras

--- InsumoQuimico/src/main/java/com/hochschild/insumoQuimico/util/FechasUtil.java
+++ InsumoQuimico/src/main/java/com/hochschild/insumoQuimico/util/FechasUtil.java
@@ -2,7 +2,9 @@ package com.hochschild.insumoQuimico.util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class FechasUtil {
     
@@ -20,5 +22,23 @@ public class FechasUtil {
 //			e.printStackTrace();
 			return null;
 		}
+    }
+    
+    public static String getPrimerDiaDelMesActual(){
+    	Calendar cal = Calendar.getInstance();
+		cal.set(cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.getActualMinimum(Calendar.DAY_OF_MONTH));
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+	        
+		return formatter.format(cal.getTime());
+    }
+    
+    public static String getUltimoDiaDelMesActual(){
+    	Calendar cal = Calendar.getInstance();
+		cal.set(cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+	        
+		return formatter.format(cal.getTime());
     }
 }
