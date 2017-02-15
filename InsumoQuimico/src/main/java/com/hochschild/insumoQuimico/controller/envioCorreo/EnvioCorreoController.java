170433c581647fa4791d42e489ff958ab0b67712
segundo.leiva <segundo.leiva@PE-L00425.hundred.com.pe>
2017-02-15 17:41:59 -0500

    mejoras

--- InsumoQuimico/src/main/java/com/hochschild/insumoQuimico/controller/envioCorreo/EnvioCorreoController.java
+++ InsumoQuimico/src/main/java/com/hochschild/insumoQuimico/controller/envioCorreo/EnvioCorreoController.java
@@ -1,7 +1,6 @@
 package com.hochschild.insumoQuimico.controller.envioCorreo;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
